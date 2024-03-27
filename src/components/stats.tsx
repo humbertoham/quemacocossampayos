249061e12b8668516e74cad1e0ef8b6857d71e1d
humbertoham <hamduranhumberto@gmail.com>
2024-03-27 14:31:57 -0600

    stats

--- src/components/stats.tsx
+++ src/components/stats.tsx
@@ -2,9 +2,8 @@
 
 
 const stats = [
-    { id: 1, name: 'Años de experiencia', value: '+2' },
-    { id: 2, name: 'Clientes Satisfechos', value: '100%' },
-    { id: 3, name: 'Clientes Atendidos', value: '+300' },
+    { id: 1, name: 'Años de experiencia', value: '16' },
+    
     
   ]
   
@@ -12,7 +11,7 @@ const stats = [
     return (
       <div className="bg-white mx-8  py-24 sm:py-32">
         <div className="mx-auto max-w-6xl py-16 rounded-3xl stat px-6 lg:px-8">
-          <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center lg:grid-cols-3">
+          <dl className="grid grid-cols-1 gap-x-8 gap-y-16 text-center ">
             {stats.map((stat) => (
               <div key={stat.id} className="mx-auto flex max-w-xs flex-col gap-y-4">
                 <dt className="text-xl leading-7 text-gray-300">{stat.name}</dt>
